60982390dae34f3baa41ea27187905b4dcd38cd8
Chase <cnickellsumn@gmail.com>
2022-09-09 15:24:59 -0700

    started hex change (22:00 on YT vid)

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,3 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    // get random number between 0 and 3 -- colors[x] (selecting all of our array of colors)
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+});
+
+function getRandomNumber() {
+    // gets random WHOLE number (rounded down with math.floor) from 0 to length of array
+    return Math.floor(Math.random() * colors.length);
+}
