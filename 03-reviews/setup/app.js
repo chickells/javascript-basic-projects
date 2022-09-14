354d8d13bbc59215dd628cc6eff19b757077b3e3
Chase <cnickellsumn@gmail.com>
2022-09-13 17:14:58 -0700

    BITCHES WE MADE IT

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -56,25 +56,65 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function() {
-  const item = reviews[currentItem];
-  
-  // YO I FINALLY LEARNED HOW THIS WORKS
-  // setting 'item' above lets me target
-  // '.img' / '.name' / etc OF that item
-  // in the array above.
-  
-  // THEN the left side targets the existing
-  // html content and replaces it with
-  // the data from the array.
-  // DAMN BRO THIS SHIT IS LIT 
-  // OKAYYYY IT'S ALL MAKING A LITTLE SENSE NOW
-  
-  // there are fucking TONS of these
-  // built in selector functions to learn tho
+  showPerson(currentItem)
+});
 
+// Commented out this out old to show my work
+//   const item = reviews[currentItem];
+  
+//   // YO I FINALLY LEARNED HOW THIS WORKS
+//   // setting 'item' above lets me target
+//   // '.img' / '.name' / etc OF that item
+//   // in the array above.
+  
+//   // THEN the left side targets the existing
+//   // html content and replaces it with
+//   // the data from the array.
+//   // DAMN BRO THIS SHIT IS LIT 
+//   // OKAYYYY IT'S ALL MAKING A LITTLE SENSE NOW
+  
+//   // there are fucking TONS of these
+//   // built in selector functions to learn tho
+
+//   img.src = item.img; 
+//   author.textContent = item.name;
+//   job.textContent = item.job;
+//   info.textContent = item.text;
+// });
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img; 
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-});
+}
 
+// show next person
+
+nextBtn.addEventListener('click', function(){
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0
+  }
+  showPerson(currentItem);
+})
+
+// show prev person
+
+prevBtn.addEventListener('click', function(){
+  currentItem--;
+  if (currentItem < 0){
+    currentItem = reviews.length - 1
+  }
+  showPerson(currentItem);
+})
+
+// show random person
+
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random() * reviews.length)
+  showPerson(currentItem);
+})
