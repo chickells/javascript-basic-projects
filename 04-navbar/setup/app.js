31f32e91a513c2d0d6c676b0d51131efd662c16f
Chase Nickells <cnickellsumn@gmail.com>
2022-09-15 07:22:27 -0700

    // create button click event // then event click ---> check if links has "show-links" // create if/then that updates class name upon click // boom, easy, let's get this bread

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// create button click event
+// then event click ---> check if links has "show-links"
+// create if/then that updates class name upon click
+// boom, easy, let's get this bread
+const toggle = document.querySelector('.nav-toggle')
+console.log(toggle)
